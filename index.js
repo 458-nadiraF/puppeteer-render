d34512c518887dc39daefc223ce7958f1ccb3fbc
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2025-04-27 21:33:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,67 @@
 const express = require("express");
-const { buy } = require("./buy");
+const { buy, bersiap } = require("./buy");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-//for send buy signal
-app.get("/buy", (req, res) => {
-  buy(req,res);
+
+let browser = null;
+let page = null;
+let browserTimeout = null;
+const headlessMode = false;
+
+// Function to launch a new browser and keep it alive for 5 minutes
+const launchBrowser = async () => {
+  browser = await puppeteer.launch({
+      headless:true,
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--no-zygote",
+        "--no-cache",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
+  page = await browser.newPage();
+
+  // Set a timeout to close the browser after 5 minutes (300000ms)
+  browserTimeout = setTimeout(async () => {
+    console.log('Browser timeout reached. Closing the browser.');
+    await browser.close();
+    browser = null;
+    page = null;
+  }, 300000); // 5 minutes
+};
+
+// Route for /bersiap
+app.get('/bersiap', async (req, res) => {
+  try {
+    if (!browser || !page) {
+      // If no browser is launched, start it
+      await launchBrowser();
+    }
+    await bersiap(page,req,res);
+    // Respond back to the client with a success message
+    res.send('Bersiap action completed!');
+    
+  } catch (error) {
+    console.error('Error in /bersiap route:', error);
+    res.status(500).send('Error performing Bersiap action');
+  }
 });
+
+// Route for /buy
+app.get('/buy', async (req, res) => {
+  try {
+    if (!browser || !page) {
+      // If no browser is launched, start it
+      await launchBrowser();
+      await bersiap(page,req,res);
+    }
+    buy(page, req,res);
+
 
 app.get("/", (req, res) => {
   res.send("Active");
