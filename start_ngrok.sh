e957d67bc6da42352c602eaadde405a5773994c8
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2025-06-15 18:11:07 +0700

    Update start_ngrok.sh

--- start_ngrok.sh
+++ start_ngrok.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# Start the Node.js application with pm2
-pm2 start /usr/src/app/index.js
+echo "[$(date)] Starting services..."
 
-# Start ngrok with the desired port (3000 in this case)
-ngrok http --url=engaging-purely-rabbit.ngrok-free.app 3000
+# Start PM2 with the Node.js app
+pm2 start index.js --name stock-monitor
+
+# Start ngrok in background
+ngrok http --url=engaging-purely-rabbit.ngrok-free.app 3000 &
+NGROK_PID=$!
+
+echo "[$(date)] Services started. PID of ngrok: $NGROK_PID"
+
+# Sleep for 8 hours
+echo "[$(date)] Will run for 8 hours..."
+sleep 28800
+
+# Stop everything
+echo "[$(date)] 8 hours elapsed. Stopping services..."
+kill $NGROK_PID
+pm2 stop all
+pm2 delete all
+
+echo "[$(date)] All services stopped."
